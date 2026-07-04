9ad4bf0cfbf7a984f892a5a3c63cad0e85851aed
Emir Utku Özgen <emirozgen293@gmail.com>
2026-07-04 18:29:59 +0300

    feat: add core encryption engine source code

---
+++ core.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class SifrelemeMotoru{
+    public:
+    virtual string sifrele(string hamMetin)=0;
+    virtual string sifreCoz(string sifreliMetin)=0;
+    virtual ~SifrelemeMotoru() {}
+};
+class SezarMotoru :public SifrelemeMotoru{
+    private:
+    int anahtar;
+    public:
+    SezarMotoru(int k):anahtar(k){}
+    string sifrele(string hamMetin){
+        string sonuc="";
+        int i;
+        for(i=0;i<hamMetin.length();i++){
+            sonuc+=char(hamMetin[i]+anahtar);
+            }
+            return sonuc;
+    }
+    string sifreCoz(string sifreliMetin){
+        string sonuc="";
+        int i;
+        for(i=0;i<sifreliMetin.length();i++){
+            sonuc+=char(sifreliMetin[i]-anahtar);
+        }
+        return sonuc;
+    }
+};
+class XorMotoru :public SifrelemeMotoru{
+    private:
+    char anahtarKarakter;
+    public:
+    XorMotoru(char karakter):anahtarKarakter(karakter){}
+    string sifrele(string hamMetin) override{
+        string sonuc="";
+        int i;
+        for(i=0;i<hamMetin.length();i++){
+            sonuc+=char(hamMetin[i]^anahtarKarakter);
+        }
+        return sonuc;
+    }
+    string sifreCoz(string sifreliMetin) override{
+        string sonuc="";
+        int i;
+        for(i=0;i<sifreliMetin.length();i++){
+            sonuc+=char(sifreliMetin[i]^anahtarKarakter);
+        }
+        return sonuc;
+    }
+};
+class GuvenliMesajMerkezi{
+    private:
+    SifrelemeMotoru* aktifMotor;
+    public:
+    GuvenliMesajMerkezi(SifrelemeMotoru* motor): aktifMotor(motor){}
+    void motoruDegistir(SifrelemeMotoru* motor){
+        aktifMotor=motor;
+        cout << "🔄 [SİSTEM] Şifreleme motoru dinamik olarak değiştirildi!" << endl;
+    }
+    void mesajGonder(string mesaj) {
+        cout << "\n📤 [MERKEZ] Gönderilecek Mesaj: " << mesaj << endl;
+        string sifreli = aktifMotor->sifrele(mesaj);
+        cout << "🔒 [AĞ] Şifreli Veri Paketlendi: " << sifreli << endl;
+        cout << "✨ [AĞ] Veri güvenli bir şekilde karşı tarafa ulaştırıldı." << endl;
+    }
+};
+
+int main() {
+    cout << "=== CryptX-Core: Güvenli Mesajlaşma Altyapısı ===" << endl;
+    cout << "=================================================" << endl;
+
+    // Motorlarımızı heap bölgesinde oluşturuyoruz kanka
+    SifrelemeMotoru* sezar = new SezarMotoru(6);
+    SifrelemeMotoru* xorMotoru = new XorMotoru('Y');
+
+    // 1. ADIM: Merkezi Sezar Motoru ile başlatıyoruz (Enjeksiyon)
+    GuvenliMesajMerkezi merkez(sezar);
+    merkez.mesajGonder("YTUE CENG Core Engineering");
+
+    cout << "\n-------------------------------------------------" << endl;
+
+    // 2. ADIM: Çalışma zamanında motoru XOR ile değiştiriyoruz!
+    merkez.motoruDegistir(xorMotoru);
+    merkez.mesajGonder("YTUE CENG Core Engineering");
+
+    // Bellek temizliği (Mühendislik ahlakı kanka, RAM'de çöp bırakmıyoruz)
+    delete sezar;
+    delete xorMotoru;
+
+    return 0;
+}
